35fa33bff7b79ac8eae675e9da9a5ad3e0cacb09
Kimjose <31962305+kimjose@users.noreply.github.com>
2023-10-27 13:39:07 +0000

    updates

--- web/analytics/referrals.php
+++ web/analytics/referrals.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Database\Capsule\Manager as DB;
 $submit = '';
 if (isset($_GET['submit'])) {
     $submit = $_GET['submit'];
@@ -32,7 +32,22 @@ if (isset($_GET['submit'])) {
     </div>
 
     <div class="card-body">
-    <div class="justify-content-center">
-    </div>
+        <div class="justify-content-center">
+            <?php 
+            if($submit):
+                $query = "select  pr.*, p.phone_no, concat(p.surname, ' ', p.first_name, ' ', p.other_names), p.dob, DATEDIFF(CURDATE(), p.dob) div 365.25 as 'currage',
+                d.name department, CONCAT(u.first_name, ' ', u.surname) as referred_by
+                from patient_referrals pr left join patients p on p.id = pr.patient_id
+                left join departments d on d.id = pr.department_id
+                left join users u on u.id = pr.created_by where pr.created_at BETWEEN '{$start_date}' and '{$end_date}' ;";
+                $referrals = DB::select($query);
+            ?>
+            <div class="table-responsive">
+                <table>
+                    
+                </table>
+            </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
